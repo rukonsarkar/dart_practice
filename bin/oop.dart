6223e63b8ba397f6503b2f66ddbf92c028fb38c0
Rukon <@rukonsarkar>
2024-03-15 23:15:25 +0600

    Updated

--- bin/oop.dart
+++ bin/oop.dart
@@ -1,8 +1,3 @@
-void main(){
- var obj = new MyClass();
- print(obj.MyName);
-}
-
 class MyClass{
 
   var MyName = 'Rukon Sarkar';
@@ -11,6 +6,32 @@ class MyClass{
   addTwoNumber(int x, int y){
     print(x+y);
   }
+}
 
+
+class Father{
+
+  BaperTaka(){
+    print("Total Taka : 10000000");
+  }
 
 }
+
+class Son extends Father{
+
+}
+
+void main(){
+  var obj = new MyClass();
+  print(obj.MyName);
+
+  var father = Father();
+  father.BaperTaka();
+
+  var son =  Son();
+  son.BaperTaka();
+
+}
+
+
+
